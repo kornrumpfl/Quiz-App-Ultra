84c07ab330a980e49155ba109c8152202769d4d2
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-26 20:34:48 +0200

    initial commit

---
+++ src/components/card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Card() {
+    return(
+        <li className="card-list__item">
+            <article class="card">
+                <h2 class="card__question">
+                    In the Kingdom Heart series who provides the english voice for Master Eraqus?
+                </h2>
+
+            </article>
+        </li>
+    );
+}
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import React from "react";
+
+export default function Header() {
+    return(
+        <div className='header'>
+            <article>
+                <h1 className="header__title">Quiz App Ultra</h1>
+            </article>
+        </div>
+    );
+}
