74e98ef3f6ba3ae17e3dfed943818ea190a8cc7a
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-28 22:26:19 +0200

    bookmarked cards

--- src/App.js
+++ src/App.js
@@ -12,30 +12,55 @@ const cards = [
     id:1234,
     question:"In the Kingdom Heart series who provides the english voice for Master Eraqus?",
     answer:"Mark Hamill",
-    tag:["#kingdomheart","#mastereraqus","#english voices"]          
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
+    bookmarked:"true"          
   },
   {
     id:4321,
     question:"Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
     answer:"Harry Potter",
-    tag:["#danielradcliffe","#hewhomustnotbenamed","#abracadabra"]          
+    tag:["#danielradcliffe","#hewhomustnotbenamed","#abracadabra"],
+    bookmarked:"false"
   },
   {
     id:2341,
     question:"Which former US president was nicknamed &quot;Teddy&quot; after he refused to shoot a defenseless black bear?",
     answer:"Theodore Roosevelt",
-    tag:["#usa","#president","#teddybear"]          
+    tag:["#usa","#president","#teddybear"],
+    bookmarked:"true"
+  },
+  {
+    id:12343,
+    question:"In the Kingdom Heart series who provides the english voice for Master Eraqus?",
+    answer:"Mark Hamill",
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
+    bookmarked:"true"          
+  },
+  {
+    id:43213,
+    question:"Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+    answer:"Harry Potter",
+    tag:["#danielradcliffe","#hewhomustnotbenamed","#abracadabra"],
+    bookmarked:"false"
+  },
+  {
+    id:23413,
+    question:"Which former US president was nicknamed &quot;Teddy&quot; after he refused to shoot a defenseless black bear?",
+    answer:"Theodore Roosevelt",
+    tag:["#usa","#president","#teddybear"],
+    bookmarked:"false"
   }
 ];
 
 function App() {
   const[showClick,setShowClick]=useState(0);
+  const[bookmarked,setBookmarked]=useState(true);
 
   return (
     <div className="App">
       <Header />
         <main className = "app__main">
-          {showClick===1?<Cards cards={cards}/>:showClick===2?<Cards cards={cards}/>:showClick===3?<Create/>:showClick===4?<Profile/>:<></>}
+          {showClick===1?<Cards cards={cards}/>:showClick===2?<Cards cards={cards.filter(card=>{return card.bookmarked==="true"})}/>:showClick===3?<Create/>:showClick===4?<Profile/>:<></>}
         </main>
       <Navigation setShowClick={setShowClick} showClick={showClick}/>
     </div>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 import Tag from "../tag/Tag";
 
 function Card({card_data}) {
-    console.log(card_data.question);
+
     const [showAnswer, setShowAnswer] = useState(false)
     return(
         <section className="card">
@@ -17,9 +17,9 @@ function Card({card_data}) {
           return <Tag tag={tag} />;
         })}
       </ul>
-      <div className="card__button-bookmark">
+      <div className= "card__button-bookmark">
         <button className="card__bookmark" aria-label="bookmark" type="button">
-          <svg className="card__bookmark-icon" viewBox="0 0 24 24">
+          <svg className={card_data.bookmarked==="true"?"card__bookmark-icon card__bookmark--active":"card__bookmark-icon"} viewBox="0 0 24 24">
             <path d="M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z" />
           </svg>
         </button>
