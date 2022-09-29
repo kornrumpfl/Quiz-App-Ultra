8442559c7cb9acf8ee8246b884dcaffe222ad969
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-29 14:17:46 +0200

    Add new Cards

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Header from "./components/header/Header";
 import Navigation from "./components/navigation/Navigation";
 import Create from './components/pages/Create';
 import Profile from './components/pages/Profile';
+import {nanoid} from "nanoid";
 
-const cards = [
+const initialCards = [
   {
     id:1234,
     question:"In the Kingdom Heart series who provides the english voice for Master Eraqus?",
@@ -19,14 +20,13 @@ const cards = [
     id:4321,
     question:"Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
     answer:"Harry Potter",
-    tag:["#danielradcliffe","#hewhomustnotbenamed","#abracadabra"],
-    bookmarked:"false"
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
   },
   {
     id:2341,
     question:"Which former US president was nicknamed &quot;Teddy&quot; after he refused to shoot a defenseless black bear?",
     answer:"Theodore Roosevelt",
-    tag:["#usa","#president","#teddybear"],
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
     bookmarked:"true"
   },
   {
@@ -40,29 +40,36 @@ const cards = [
     id:43213,
     question:"Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
     answer:"Harry Potter",
-    tag:["#danielradcliffe","#hewhomustnotbenamed","#abracadabra"],
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
     bookmarked:"false"
   },
   {
     id:23413,
     question:"Which former US president was nicknamed &quot;Teddy&quot; after he refused to shoot a defenseless black bear?",
     answer:"Theodore Roosevelt",
-    tag:["#usa","#president","#teddybear"],
+    tag:["#kingdomheart","#mastereraqus","#english voices"],
     bookmarked:"false"
   }
 ];
 
+
 function App() {
   const[showClick,setShowClick]=useState(0);
+  const[cards,setCards]=useState(initialCards);
   
+  function appendCard(question, answer, ...tag){
+    console.log(tag);
+      setCards([...cards,{id:nanoid(),question:question,answer:answer,tag:tag,bookmarked:false}])
+  }
 
   return (
     <div className="App">
       <Header />
         <main className = "app__main">
-          {showClick===1?<Cards cards={cards}/>:showClick===2?<Cards cards={cards.filter(card=>{return card.bookmarked==="true"})}/>:showClick===3?<Create/>:showClick===4?<Profile/>:<></>}
+          {showClick===1?<Cards cards={cards}/>:showClick===2?<Cards cards={cards.filter(card=>{return card.bookmarked==="true"})}/>:showClick===3?<div><Create onHandleSubmit={appendCard}/></div>:showClick===4?<Profile/>:<></>}
         </main>
       <Navigation setShowClick={setShowClick} showClick={showClick}/>
+      
     </div>
   );
 }
--- src/components/pages/Create.js
+++ src/components/pages/Create.js
@@ -1,14 +1,42 @@
 import Icon from "./Icon";
+import '../pages/Create.css'
 import './Icon.css';
 
-function Create(){
+function Create({onHandleSubmit}){
+    function onSubmit(event){
+        event.preventDefault();
+        const form = event.target;
+        const {question} = form.elements;
+        const {answer} = form.elements;
+        const {tag1} = form.elements;
+        const {tag2} = form.elements;
+        const {tag3} = form.elements;
+        onHandleSubmit(question.value,answer.value,tag1.value,tag2.value,tag3.value);
+        form.reset();
+    }
     return(
-        <div >
-        <h1 className="text">Profile / Create page is currently under construction</h1>
-            <div className="size">
-                <Icon/>
-            </div>
-        </div>
+
+        <form onSubmit = {onSubmit} className="form" aria-label="Add a new question">
+            <label htmlFor="question">Your question:</label>
+                        <textarea
+                            id="question"
+                            rows="7"
+                            name="question"
+                            maxLength="150"
+                        ></textarea>
+                        <label htmlFor="answer">Your answer:</label>
+                        <textarea
+                            id="answer"
+                            rows="7"
+                            name="answer"
+                            data-js="input-answer"
+                        ></textarea>
+                        <label htmlFor="tag">Tag:</label>
+                        <input id="tag1" type="text" name="tag1" />
+                        <input id="tag2" type="text" name="tag2" />
+                        <input id="tag3" type="text" name="tag3" />
+                        <button className="form__submit-button" type="submit">Submit</button>
+        </form>
     );
 }
 export default Create;
